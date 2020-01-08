ae3812abe94ce9c1e201a94db17fd87699b47e67
Nick Wang <nickyiliwang@gmail.com>
2020-01-08 05:02:51 -0500

    Header component

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/Header"
+
+export default () => (
+  <div style={{ color: "blue", fontSize: `72px` }}>
+    <Header />
+    <h2>HEHEHEH</h2>
+    <p>What do you meannnnnn</p>
+  </div>
+)
